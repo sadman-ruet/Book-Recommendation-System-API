a0492a0ef3c0f6cbebb88745513ef532de1acdde
sadman-ruet <sadmanhasib21@gmail.com>
2025-03-10 13:45:55 +0600

    final

--- database.py
+++ database.py
@@ -1,8 +1,14 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 def initialize_db(database:str):
-    uri = "mongodb+srv://sadmanhasib:sadmanhasib@cluster0.fdf5z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    try:
+        uri = os.getenv("MONGODB_URL")
+    except:
+        uri = "mongodb://localhost:27017/"
     client = MongoClient(uri, server_api=ServerApi('1'))
     return client[database]
 
