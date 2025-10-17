7d2ee2ecbab9cc671897fbd99975be5097358a7c
sadman-ruet <sadmanhasib21@gmail.com>
2025-10-17 11:02:37 +0600

    Deleting all files

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from llm import model
-from routers import user
-app = FastAPI()
-app.include_router(user.router)
-@app.get("/")
-def index():
-    return {"message": "Hello World"}
--- schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-
-class Input(BaseModel):
-    query: str
-class Output(BaseModel):
-    suggestions: List[str]
-    details: List[str]
